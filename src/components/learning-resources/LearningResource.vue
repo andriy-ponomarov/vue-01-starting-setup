8a421b418b71fc4f0a7bc82260dd6cf1700bb963
Andriy Ponomarov <andriy.ponomarov@arag.at>
2026-06-22 14:36:07 +0200

    First changes with list of resources

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <div>
+      <hearer>
+        <h3>
+          {{ title }}
+        </h3>
+        <button>Delete</button>
+      </hearer>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link" target="_blank">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+};
+</script>
